7e4a12296f18936f3eafe76877546cd11941ffa8
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2023-01-11 11:54:56 +0100

    working on mocking and spies

--- 07-mocking-spies/00-NodeJS-project-side-effects/src/util/io.js
+++ 07-mocking-spies/00-NodeJS-project-side-effects/src/util/io.js
@@ -6,3 +6,12 @@ export default function writeData(data, filename) {
   const storagePath = path.join(process.cwd(), "data", filename);
   return fs.writeFile(storagePath, data);
 }
+
+//% mocks
+//| here we have the problem that we do not want to execute the original writeFile method
+//| we only want to find out whether it was called, but it should not do its job of writing to the file system
+//| it should do that during production, but not when we run our tests
+//| we could use spies, but to replace it with an empty function and find out whether it was called it is harder
+//| because it comes from a module that we do not own
+//| IMPORTANT with mocks we can replace functionalities that are defined in modules,
+//| regardless of whether we own them or not
--- 07-mocking-spies/00-NodeJS-project-side-effects/src/util/io.test.js
+++ 07-mocking-spies/00-NodeJS-project-side-effects/src/util/io.test.js
@@ -1,6 +1,8 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
 import writeData from "./io";
+import { promises as fs } from "fs"; // import mocked module because we used vi.mock("fs")
 
+/*
 it("should execute the writeFile mehtod", () => {
   const testData = "Test";
   const testFilename = "test.txt";
@@ -10,6 +12,7 @@ it("should execute the writeFile mehtod", () => {
   //| but we would not make it here if it was rejected
   return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
 });
+*/
 //| this has a disadvantage: whenever we run this test, we create the test.txt file in the data folder
 //| sometimes this might be a problem
 //| because our test has a side effect, i.e. it writes data to the hard drive
@@ -39,3 +42,33 @@ it("should execute the writeFile mehtod", () => {
 |NB for spies --> cfr data.js and data.test.js
 |NB for mocks --> cfr io.js and io.test.js
 */
+
+//% mocks
+//| here we have the problem that we do not want to execute the original writeFile method
+//| we only want to find out whether it was called, but it should not do its job of writing to the file system
+//| it should do that during production, but not when we run our tests
+//| we could use spies, but to replace it with an empty function and find out whether it was called it is harder
+//| because it comes from a module that we do not own
+//| IMPORTANT with mocks we can replace functionalities that are defined in modules,
+//| regardless of whether we own them or not
+
+//| we have the .mock() method on the vi object
+//| this .mock() method gets as input argument the name of the module or
+//| the path to the module that should be mocked
+//| this works with builtin or third party module but also with your own modules / files
+//| this will kick off the Vitest or Jest auto-mocking algorithm
+//| it will basically find this module, and replace all the functions in there with empty spy functions that do not do anything
+vi.mock("fs");
+//| now when we call writeData, it will no longer write the test.txt file
+
+it("should execute the writeFile mehtod", () => {
+  const testData = "Test";
+  const testFilename = "test.txt";
+
+  //| however, now we no longer get back a promise that resolves (because now there are only empty spy functions)
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+  writeData(testData, testFilename);
+  // now fs.writeFile is an empty spy function therefore we can call (NB it is an automatically created spy function)
+  expect(fs.writeFile).toBeCalled();
+});
